d6022d084fb0697589f927c73b3bec036509c15f
Grant <gergely.daniel7@gmail.com>
2018-04-08 02:05:03 +0200

    Add result charts and other functionality

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: {
+		index: './src/index.js',
+		thanks: './src/thanks.js',
+		result: './src/result.js',
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: '[name].bundle.js'
 	},
 	module: {
 		rules: [
@@ -21,6 +25,14 @@ module.exports = {
 					loader: "css-loader" // translates CSS into CommonJS
 				}, {
 					loader: "sass-loader" // compiles Sass to CSS
+				}]
+			},
+			{
+				test: /\.css$/,
+				use: [{
+					loader: "style-loader" // creates style nodes from JS strings
+				}, {
+					loader: "css-loader" // translates CSS into CommonJS
 				}]
 			},
 			{
